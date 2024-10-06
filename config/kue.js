fd31f5ae575275df93ed7272638f2cbe77f5fa33
Aryan raj <41327181+aryanbond007@users.noreply.github.com>
2024-10-07 02:00:19 +0530

    Update kue.js

--- config/kue.js
+++ config/kue.js
@@ -1,4 +1,14 @@
+// const kue = require('kue');
+// const queue = kue.createQueue();
+
+// module.exports = queue;
 const kue = require('kue');
-const queue = kue.createQueue();
+const queue = kue.createQueue({
+  redis: {
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD || undefined // use undefined if not required
+  }
+});
 
 module.exports = queue;
